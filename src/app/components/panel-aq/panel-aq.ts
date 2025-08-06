f4b30a50ce13ed29d6229b6a04f6c468cffd590a
Danny <danny@practicas.mnxonline.com>
2025-08-06 13:18:05 +0100

    feat:added auto position;fix:swagger doc didn't open

--- src/app/components/panel-aq/panel-aq.ts
+++ src/app/components/panel-aq/panel-aq.ts
@@ -3,12 +3,15 @@ import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
 import { MatOption, MatSelect } from '@angular/material/select';
-import { MatSpinner } from '@angular/material/progress-spinner';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FormsModule } from '@angular/forms';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { AirQualityService } from '../../../services/air-quality.service';
 import { tap } from 'rxjs/operators';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { GeolocationService } from '../../services/geoLocation/geoLocation';
+import { MatButton } from '@angular/material/button';
+import { createGlobalPositionStrategy } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-panel-aq',
@@ -19,31 +22,89 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
     MatLabel,
     MatSelect,
     MatOption,
-    MatSpinner,
+    MatProgressSpinnerModule,
     FormsModule,
     NgxSkeletonLoaderModule,
     MatInput,
-    MatAutocompleteModule
+    MatAutocompleteModule,
+    MatButton
   ],
   templateUrl: './panel-aq.html',
   styleUrls: ['./panel-aq.scss'],
 })
 export class PanelAq {
+
   elemts = signal<any[]>([]);
   selectedId = signal<number | null>(null);
   parameters = signal<any[]>([]);
   loading = signal(false);
+  isPosition = signal(false);
 
   inputValue = ''
   filteredRes: any[] = []
 
   constructor(
     private http: HttpClient,
-    private airQualityService: AirQualityService
+    private airQualityService: AirQualityService,
+    private geo: GeolocationService
   ) { }
 
-  ngOnInit() {
+  togglePositionBbox() {
+    console.log('se cambia a Bbox')
+    this.isPosition.set(true)
+    this.fetchFromPosition();
+  }
+
+  togglePositionDefault(){
+    console.log('se cambia a default');
+    this.isPosition.set(false);
     this.fetchData();
+  }
+
+  ngOnInit() {
+    if (this.isPosition()) {
+      this.fetchFromPosition()
+      console.log('fromPosition')
+    }
+    this.fetchData();
+    console.log('byDefault')
+  }
+
+
+  processBoundingBox(): Promise<[number, number, number, number]> {
+    return this.geo.getCurrentPosition().then(position => {
+      const { latitude, longitude } = position.coords;
+      const bboxObj = this.getBoundingBox(latitude, longitude, 0.5);
+      const bbox: [number, number, number, number] = [
+        bboxObj.minLon,
+        bboxObj.minLat,
+        bboxObj.maxLon,
+        bboxObj.maxLat
+      ];
+      console.log(bbox)
+      return bbox;
+    });
+  }
+
+  getBoundingBox(lat: number, lon: number, delta: number) {
+    return {
+      minLat: lat - delta,
+      maxLat: lat + delta,
+      minLon: lon - delta,
+      maxLon: lon + delta,
+    };
+  }
+
+  fetchFromPosition() {
+    this.processBoundingBox().then(bbox => {
+      this.airQualityService.sendBbox(bbox).subscribe({
+        next: response => {
+          console.log('Air Quality Data:', response);
+          this.elemts.set(response)
+        },
+        error: (err) => console.error('Error al obtener datos:', err)
+      });
+    });
   }
 
   fetchData() {
@@ -79,7 +140,7 @@ export class PanelAq {
 
   filter() {
     const input = this.inputValue.toLowerCase();
-    this.filteredRes = this.elemts().filter (elemt => elemt.name.toLowerCase().includes(input))
+    this.filteredRes = this.elemts().filter(elemt => elemt.name.toLowerCase().includes(input))
     console.log(this.filteredRes)
   }
 }
--- src/services/air-quality.service.ts
+++ src/services/air-quality.service.ts
@@ -7,9 +7,14 @@ import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common
 export class AirQualityService {
   private backendUrl = 'http://localhost:3000/air-quality';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   fetchFromApiById(id: number) {
     return this.http.get(`${this.backendUrl}/locations/fetch?id=${id}`);
+  }
+
+  sendBbox(bbox: [number, number, number, number]) {
+    const bboxStr = bbox.join(',');
+    return this.http.get<any[]>(`${this.backendUrl}/bbox?bbox=${bboxStr}`);
   }
 }
