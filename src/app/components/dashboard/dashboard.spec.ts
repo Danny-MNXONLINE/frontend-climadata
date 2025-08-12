38aa037b3de60e67abc74c81cdea9e6080358146
DannyMNX-ONLINE <danny@practicas.mnxonline.com>
2025-08-12 12:43:06 +0100

    arreglado test dashboard

--- src/app/components/dashboard/dashboard.spec.ts
+++ src/app/components/dashboard/dashboard.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Dashboard } from './dashboard';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+
 
 describe('Dashboard', () => {
   let component: Dashboard;
@@ -8,9 +11,24 @@ describe('Dashboard', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Dashboard]
-    })
-    .compileComponents();
+      imports: [
+        provideHttpClientTesting, // para HttpClient si usas
+      ],
+      declarations: [Dashboard],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ id: 123 }), // mockea params o lo que uses
+            snapshot: {
+              paramMap: {
+                get: () => '123'
+              }
+            }
+          }
+        }
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Dashboard);
     component = fixture.componentInstance;
