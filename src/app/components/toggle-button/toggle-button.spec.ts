a42b5831b0d0d8dcb50b7a07b8677f2eb61d35ba
DannyMNX-ONLINE <danny@practicas.mnxonline.com>
2025-08-12 12:09:12 +0100

    maybe fix:ToggleTheme test

--- src/app/components/toggle-button/toggle-button.spec.ts
+++ src/app/components/toggle-button/toggle-button.spec.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ToggleButton } from './toggle-button';
+import { ThemeService } from '../../services/toggle-theme';
 
 describe('ToggleButton', () => {
   let component: ToggleButton;
   let fixture: ComponentFixture<ToggleButton>;
+  let themeService: ThemeService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +15,17 @@ describe('ToggleButton', () => {
 
     fixture = TestBed.createComponent(ToggleButton);
     component = fixture.componentInstance;
+    themeService = TestBed.inject(ThemeService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call toggleTheme on themeService when toggleTheme is called', () => {
+    spyOn(themeService, 'toggleTheme');
+    component.toggleTheme();
+    expect(themeService.toggleTheme).toHaveBeenCalled();
   });
 });
