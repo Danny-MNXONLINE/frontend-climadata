24236a03fadc4affd352623c6126799b8076275a
DannyMNX-ONLINE <danny@practicas.mnxonline.com>
2025-08-18 10:05:32 +0100

    fix: separate ActivatedRoute import from Landing component imports in tests

--- src/app/components/landing/landing.spec.ts
+++ src/app/components/landing/landing.spec.ts
@@ -8,7 +8,8 @@ describe('Landing', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Landing, ActivatedRoute]
+      imports: [Landing],
+      providers: [ActivatedRoute]
     })
     .compileComponents();
 
