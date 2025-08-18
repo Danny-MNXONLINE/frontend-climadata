af8b24c89f1a26ac48af80a1aad9458f2fceeffb
DannyMNX-ONLINE <danny@practicas.mnxonline.com>
2025-08-18 10:02:53 +0100

    fix: add ActivatedRoute import to Landing component tests

--- src/app/components/landing/landing.spec.ts
+++ src/app/components/landing/landing.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Landing } from './landing';
+import { ActivatedRoute } from '@angular/router';
 
 describe('Landing', () => {
   let component: Landing;
@@ -8,7 +8,7 @@ describe('Landing', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Landing]
+      imports: [Landing, ActivatedRoute]
     })
     .compileComponents();
 
