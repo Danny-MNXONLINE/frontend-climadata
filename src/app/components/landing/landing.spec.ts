e3f5e3e1d7ca34edee26e77e800fc84f66a9daf3
DannyMNX-ONLINE <danny@practicas.mnxonline.com>
2025-08-18 10:08:20 +0100

    fix: update tests to provide ActivatedRoute with mock values for Landing and Aside components

--- src/app/components/landing/landing.spec.ts
+++ src/app/components/landing/landing.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Landing } from './landing';
 import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('Landing', () => {
   let component: Landing;
@@ -9,9 +10,20 @@ describe('Landing', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Landing],
-      providers: [ActivatedRoute]
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ id: 123 }), // mockea params o lo que uses
+            snapshot: {
+              paramMap: {
+                get: () => '123'
+              }
+            }
+          }
+        }]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(Landing);
     component = fixture.componentInstance;
--- src/app/layouts/aside/aside.spec.ts
+++ src/app/layouts/aside/aside.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Aside } from './aside';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('Aside', () => {
   let component: Aside;
@@ -8,9 +9,22 @@ describe('Aside', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Aside]
+      imports: [Aside],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ id: 123 }), // mockea params o lo que uses
+            snapshot: {
+              paramMap: {
+                get: () => '123'
+              }
+            }
+          }
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(Aside);
     component = fixture.componentInstance;
