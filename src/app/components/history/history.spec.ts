65ea40f118ad783cb004e2e011b8d7a8183507ed
DannyMNX-ONLINE <danny@practicas.mnxonline.com>
2025-08-19 14:13:58 +0100

    fix: add HTTP client providers to History component tests

--- src/app/components/history/history.spec.ts
+++ src/app/components/history/history.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { HistoryComponent } from './history';
 
 describe('History', () => {
@@ -8,7 +9,8 @@ describe('History', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HistoryComponent]
+      imports: [HistoryComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()]
     })
     .compileComponents();
 
